76d9251113a3774d5e0f3edb0d56bc83bc22ad2c
Anthony Tedja <anthonytedja27@gmail.com>
2025-01-16 03:03:27 -0500

    Add MVP Code

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,123 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useEffect, useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
+
+export default function Index() {
+  const workerRef = useRef<Worker>();
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [loading, setLoading] = useState(true);
+  const [hidden, setHidden] = useState(false);
+
+  type UncompressMessage = {
+    action: "uncompress_each" | "error" | "ready";
+    file_name: string;
+    url: string;
+    index: number;
+    size: number;
+  };
+
+  useEffect(() => {
+    workerRef.current = new Worker(
+      new URL("../lib/worker.js", import.meta.url)
+    );
+    workerRef.current.onmessage = (e: MessageEvent<UncompressMessage>) => {
+      switch (e.data.action) {
+        case "uncompress_each":
+          const file_name = e.data.file_name;
+          const url = e.data.url;
+          const index = e.data.index;
+
+          // add the image to the page
+          const img = document.createElement("img");
+          img.src = url;
+          img.alt = file_name;
+          img.id = `image-${index}`;
+          if (index > 3) {
+            img.loading = "lazy";
+          }
+
+          const images = document.getElementById("images");
+          img.classList.add("w-full");
+          images?.appendChild(img);
+          break;
+        case "error":
+          break;
+        case "ready":
+          setLoading(false);
+          break;
+      }
+    };
+    return () => {
+      workerRef.current?.terminate();
+    };
+  }, []);
+
+  const onFileSelected = async (e: React.FormEvent<HTMLInputElement>) => {
+    if (!e.currentTarget?.files) return;
+    const file = e.currentTarget.files[0];
+    if (!file) return;
+
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+
+    if (![".cbr", ".cbz", ".cbt"].includes(file.name.slice(-4))) {
+      alert("Invalid file type");
+      return;
+    }
+
+    const blob = file.slice();
+    const file_name = file.name;
+
+    // Convert the blob into an array buffer
+    const reader = new FileReader();
+    reader.onload = function () {
+      const array_buffer = reader.result;
+
+      // Send the file name and array buffer to the web worker
+      const message = {
+        action: "uncompress_start",
+        file_name: file_name,
+        array_buffer: array_buffer,
+      };
+
+      workerRef.current?.postMessage(message);
+    };
+    reader.readAsArrayBuffer(blob);
+
+    setHidden(true);
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[0px_1fr_0px] items-center justify-items-center min-h-screen p-1 sm:p-10 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+        <input
+          type="file"
+          tabIndex={-1}
+          ref={inputRef}
+          onChange={onFileSelected}
+          accept=".cbr,.cbz,.cbt"
+          hidden
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <Button
+          disabled={loading}
+          variant="outline"
+          className={hidden ? "hidden" : ""}
+          onClick={(e) => {
+            e.stopPropagation();
+            inputRef.current?.click();
+          }}
+        >
+          Select File
+        </Button>
+        <div
+          id="images"
+          className={`flex flex-col justify-center max-w-screen-lg gap-1 ${
+            !hidden ? "hidden" : ""
+          } `}
+        ></div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
